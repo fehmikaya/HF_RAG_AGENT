7e132d221e13dd9aa26379e8d339355df06361a3
Fehmi Kaya <fehmikaya@users.noreply.huggingface.co>
2024-06-04 19:19:03 +0000

    Create ragagent.py

---
+++ ragagent.py
@@ -0,0 +1,22 @@
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+class RAGAgent():
+
+    def __init__(self, docs):
+        docs_list = [item for sublist in docs for item in sublist]
+    
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=250, chunk_overlap=0
+        )
+        doc_splits = text_splitter.split_documents(docs_list)
+        
+        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        
+        # Add to vectorDB
+        vectorstore = Chroma.from_documents(
+            documents=doc_splits,
+            collection_name="rag-chroma",
+            embedding=embedding_function,
+        )
+        self.retriever = vectorstore.as_retriever()
