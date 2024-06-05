7f463bdc648c1e8c9424c87b0ea47a56d0590f6a
Fehmi Kaya <fehmikaya@users.noreply.huggingface.co>
2024-06-05 19:15:27 +0000

    Update ragagent.py

--- ragagent.py
+++ ragagent.py
@@ -1,23 +1,297 @@
+
+###   RAG Agent with Langchain and Langgraph, Hallucination and Sanity Checks with Websearch 
+
+from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+from langgraph.graph import END, StateGraph
+
+from typing_extensions import TypedDict
+from typing import List
+from langchain_core.documents import Document
+import os
+
 
 class RAGAgent():
 
     def __init__(self, docs):
         docs_list = [item for sublist in docs for item in sublist]
-    
+
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
             chunk_size=250, chunk_overlap=0
         )
         doc_splits = text_splitter.split_documents(docs_list)
-        
+
         embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        
+
         # Add to vectorDB
         vectorstore = Chroma.from_documents(
             documents=doc_splits,
             collection_name="rag-chroma",
             embedding=embedding_function,
         )
-        self.retriever = vectorstore.as_retriever()
+        RAGAgent.retriever = vectorstore.as_retriever()
+
+    os.environ["TAVILY_API_KEY"] = userdata.get('TV_TOKEN')
+
+    HF_TOKEN = os.getenv("HF_TOKEN")
+    TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+    
+    if HF_TOKEN is None:
+        st.error("API key not found. Please set the HF_TOKEN secret in your Hugging Face Space.")
+        st.stop()
+    if TAVILY_API_KEY is None:
+        st.error("API key not found. Please set the TAVILY_API_KEY secret in your Hugging Face Space.")
+        st.stop()
+
+    router_llm = CustomLlama3(bearer_token = HF_TOKEN)
+    router_prompt = PromptTemplate(
+        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an expert at routing a 
+        user question to a vectorstore or web search. Use the vectorstore for questions on LLM  agents, 
+        prompt engineering, and adversarial attacks. You do not need to be stringent with the keywords 
+        in the question related to these topics. Otherwise, use web-search. Give a binary choice 'web_search' 
+        or 'vectorstore' based on the question. Return the a JSON with a single key 'datasource' and 
+        no premable or explanation. Question to route: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+        input_variables=["question"],
+    )
+    question_router = router_prompt | router_llm | JsonOutputParser()
+
+    retrieval_grader_prompt = PromptTemplate(
+        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance
+        of a retrieved document to a user question. If the document contains keywords related to the user question,
+        grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+        Provide the binary score as a JSON with a single key 'score' and no premable or explanation.
+        <|eot_id|><|start_header_id|>user<|end_header_id|>
+        Here is the retrieved document: \n\n {document} \n\n
+        Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+        """,
+        input_variables=["question", "document"],
+    )
+    retrieval_grader_llm = CustomLlama3(bearer_token = HF_TOKEN)
+    retrieval_grader = retrieval_grader_prompt | retrieval_grader_llm | JsonOutputParser()
+
+    answer_prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Question: {question} 
+    Context: {context} 
+    Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question", "document"],
+    )
+    answer_llm = CustomLlama3(bearer_token = HF_TOKEN)
+
+    # Post-processing
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    rag_chain = answer_prompt | answer_llm | StrOutputParser()
+
+    hallucination_prompt = PromptTemplate(
+    template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
+    an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate 
+    whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+    single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Here are the facts:
+    \n ------- \n
+    {documents} 
+    \n ------- \n
+    Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "documents"],
+    )
+    hallucination_llm = CustomLlama3(bearer_token = HF_TOKEN)
+    hallucination_grader = hallucination_prompt | hallucination_llm | JsonOutputParser()
+
+    answer_grader_prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether an 
+    answer is useful to resolve a question. Give a binary score 'yes' or 'no' to indicate whether the answer is 
+    useful to resolve a question. Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
+     <|eot_id|><|start_header_id|>user<|end_header_id|> Here is the answer:
+    \n ------- \n
+    {generation} 
+    \n ------- \n
+    Here is the question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "question"],
+    )
+    answer_grader_llm = CustomLlama3(bearer_token = HF_TOKEN)
+    answer_grader = answer_grader_prompt | answer_grader_llm | JsonOutputParser()
+
+    web_search_tool = TavilySearchResults(k=3)
+
+    class GraphState(TypedDict):
+      question: str
+      generation: str
+      web_search: str
+      documents: List[str]
+    
+    def retrieve(state):
+        print("---RETRIEVE---")
+        question = state["question"]
+
+        # Retrieval
+        documents = RAGAgent.retriever.invoke(question)
+        return {"documents": documents, "question": question}
+
+    def generate(state):
+        print("---GENERATE---")
+        question = state["question"]
+        documents = state["documents"]
+
+        # RAG generation
+        generation = RAGAgent.rag_chain.invoke({"context": documents, "question": question})
+        return {"documents": documents, "question": question, "generation": generation}
+
+
+    def grade_documents(state):
+
+        print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+        question = state["question"]
+        documents = state["documents"]
+
+        # Score each doc
+        filtered_docs = []
+        web_search = "No"
+        for d in documents:
+            score = RAGAgent.retrieval_grader.invoke(
+                {"question": question, "document": d.page_content}
+            )
+            grade = score["score"]
+            # Document relevant
+            if grade.lower() == "yes":
+                print("---GRADE: DOCUMENT RELEVANT---")
+                filtered_docs.append(d)
+            # Document not relevant
+            else:
+                print("---GRADE: DOCUMENT NOT RELEVANT---")
+                # We do not include the document in filtered_docs
+                # We set a flag to indicate that we want to run web search
+                web_search = "Yes"
+                continue
+        return {"documents": filtered_docs, "question": question, "web_search": web_search}
+
+
+    def web_search(state):
+
+        print("---WEB SEARCH---")
+        question = state["question"]
+        documents = state["documents"]
+
+        # Web search
+        docs = RAGAgent.web_search_tool.invoke({"query": question})
+        web_results = "\n".join([d["content"] for d in docs])
+        web_results = Document(page_content=web_results)
+        if documents is not None:
+            documents.append(web_results)
+        else:
+            documents = [web_results]
+        return {"documents": documents, "question": question}
+
+
+    def route_question(state):
+
+        print("---ROUTE QUESTION---")
+        question = state["question"]
+        print(question)
+        source = RAGAgent.question_router.invoke({"question": question})
+
+        if source["datasource"] == "web_search":
+            print("---ROUTE QUESTION TO WEB SEARCH---")
+            return "websearch"
+        elif source["datasource"] == "vectorstore":
+            print("---ROUTE QUESTION TO RAG---")
+            return "vectorstore"
+
+
+    def decide_to_generate(state):
+
+        print("---ASSESS GRADED DOCUMENTS---")
+        question = state["question"]
+        web_search = state["web_search"]
+        filtered_documents = state["documents"]
+
+        if web_search == "Yes":
+            # All documents have been filtered check_relevance
+            # We will re-generate a new query
+            print(
+                "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+            )
+            return "websearch"
+        else:
+            # We have relevant documents, so generate answer
+            print("---DECISION: GENERATE---")
+            return "generate"
+
+    def grade_generation_v_documents_and_question(state):
+
+        print("---CHECK HALLUCINATIONS---")
+        question = state["question"]
+        documents = state["documents"]
+        generation = state["generation"]
+
+        score = RAGAgent.hallucination_grader.invoke(
+            {"documents": documents, "generation": generation}
+        )
+        grade = score["score"]
+
+        # Check hallucination
+        if grade == "yes":
+            print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+            # Check question-answering
+            print("---GRADE GENERATION vs QUESTION---")
+            score = RAGAgent.answer_grader.invoke({"question": question, "generation": generation})
+            grade = score["score"]
+            if grade == "yes":
+                print("---DECISION: GENERATION ADDRESSES QUESTION---")
+                return "useful"
+            else:
+                print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+                return "not useful"
+        else:
+            print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+            return "not supported"
+
+    workflow = StateGraph(GraphState)
+
+    # Define the nodes
+    workflow.add_node("websearch", web_search)  # web search
+    workflow.add_node("retrieve", retrieve)  # retrieve
+    workflow.add_node("grade_documents", grade_documents)  # grade documents
+    workflow.add_node("generate", generate)  # generatae
+
+    # Build graph
+    workflow.set_conditional_entry_point(
+        route_question,
+        {
+            "websearch": "websearch",
+            "vectorstore": "retrieve",
+        },
+    )
+
+    workflow.add_edge("retrieve", "grade_documents")
+    workflow.add_conditional_edges(
+        "grade_documents",
+        decide_to_generate,
+        {
+            "websearch": "websearch",
+            "generate": "generate",
+        },
+    )
+    workflow.add_edge("websearch", "generate")
+    workflow.add_conditional_edges(
+        "generate",
+        grade_generation_v_documents_and_question,
+        {
+            "not supported": "generate",
+            "useful": END,
+            "not useful": "websearch",
+        },
+    )
+
+    # Compile
+    app = workflow.compile()  
