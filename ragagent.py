c137dfe4fad4e548bcef5057e27c96c0560a9523
Fehmi Kaya <fehmikaya@users.noreply.huggingface.co>
2024-06-11 13:11:24 +0000

    Update ragagent.py

--- ragagent.py
+++ ragagent.py
@@ -25,7 +25,7 @@ class RAGAgent():
         docs_list = [item for sublist in docs for item in sublist]
 
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size=250, chunk_overlap=0
+            chunk_size=1000, chunk_overlap=200
         )
         doc_splits = text_splitter.split_documents(docs_list)
 
